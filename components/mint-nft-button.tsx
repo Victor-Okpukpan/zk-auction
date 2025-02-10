76d52b5ae6af33fbea06182cc47a80e6202c9264
Victor Okpukpan <vokpukpan@gmail.com>
2025-02-10 10:36:42 +0100

    feat: fetch connected user's NFTs on Arbitrum

--- components/mint-nft-button.tsx
+++ components/mint-nft-button.tsx
@@ -1,7 +1,29 @@
 "use client";
+import { nftAbi } from "@/lib/NftAbi";
+import { useWriteContract } from "wagmi";
 
 export default function MintNftButton() {
+  const { writeContractAsync } = useWriteContract();
+
+  const mintNft = async () => {
+    try {
+      const result = await writeContractAsync({
+        abi: nftAbi,
+        address: "0xcB26E956ba06d77dea887d74592223148dC9D08c",
+        functionName: "mintNft",
+      });
+
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <button className="flex items-center gap-2 rounded-lg bg-primary px-4 py-3 font-medium text-primary-foreground transition-colors hover:bg-primary/90">Mint NFT</button>
-  )
+    <button
+      onClick={mintNft}
+      className="flex items-center gap-2 rounded-lg bg-primary px-4 py-3 font-medium text-primary-foreground transition-colors hover:bg-primary/90"
+    >
+      Mint NFT
+    </button>
+  );
 }
--- hooks/useNFTs.ts
+++ hooks/useNFTs.ts
@@ -6,7 +6,7 @@ import { Alchemy, Network } from 'alchemy-sdk';
 
 const config = {
   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
+  network: Network.ARB_SEPOLIA,
 };
 
 const alchemy = new Alchemy(config);
